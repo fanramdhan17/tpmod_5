020df4f866e444525346e76d7b932e1199919ff4
luthfanrw17 <olioaglio55@gmail.com>
2022-03-25 17:30:57 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302204038/tpmodul5_1302204038/Program.cs
+++ tpmodul5_1302204038/tpmodul5_1302204038/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace tpmodul5_1302204038
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Fan");
+            video.PrintVideoDetails();
+            video.IncreasePlayCount(1);
+            video.PrintVideoDetails();
+        }
+    }
+
+    public class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int PlayCount;
+
+        public SayaTubeVideo(String judul)
+        {
+            Random ids = new Random();
+            this.title = title;
+            id = ids.Next(0, 100000);
+            this.PlayCount = 0;
+        }
+
+        public void IncreasePlayCount(int i)
+        {
+            PlayCount = PlayCount + i;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine(this.id);
+            Console.WriteLine(this.title);
+            Console.WriteLine(this.PlayCount);
+        }
+    }
+}
